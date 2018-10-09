50342585ffb6c49cf5df7d22b0d97fff0505653f
racheliwhite01 <racheliwhite01@gmail.com>
2018-10-09 08:35:37 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,18 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles',]
+
+function addElementToArray(array, element) {
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(Array,element) {
+  chocolateBars.unshift()
+}
+
+function addElementToEndOfArray() {
+  
+}
+
+function DestructivelyAddElementToEndOfArray() {
+  
+}
+
