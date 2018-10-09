438e2f9581e82831891dbdaf983dc631803c555e
racheliwhite01 <racheliwhite01@gmail.com>
2018-10-09 08:39:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles',]
 
-function addElementToArray(array, element) {
+function destructivelyAddElementToBeginningOfArray(array, element) {
   (chocolateBars).push('smarties')
 }
 
